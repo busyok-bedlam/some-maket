090fc44213a30d4bb644b295f03760d0e3e70412
busyok-bedlam <quouple@mail.ru>
2017-04-30 11:44:44 +0300

    second commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,27 @@
 var gulp = require('gulp');
 var style = require('gulp-stylus');
 var jade = require("gulp-jade");
-var pref = require("gulp-autoprefixer");
-var bs 	= require("browser-sync").create();
+const pref  = require('gulp-autoprefixer');
+
 gulp.task('style-compile',function () {
-	gulp.src('./app/style/*styl').pipe(style()).pipe(pref({pretty:true})).pipe(gulp.dest('./app/public/'));
+	gulp.src('./app/style/*styl').pipe(style()).pipe(pref()).pipe(gulp.dest('./app/public/'));
+	
 })
 gulp.task('jade-compile',function(){
 	gulp.src('./app/page/*jade').pipe(jade({pretty:true})).pipe(gulp.dest("./"))
 })
-
+gulp.task('prefix',function(){
+	gulp.src('./app/public/*css').pipe(pref({
+		browsers: ['last 2 versions'],
+  		cascade: false
+	})).pipe(gulp.dest('./app/public/'))
+})
 gulp.task('wach', function(){
 	gulp.watch("./app/style/*styl",['style-compile'])
 	gulp.watch("./app/page/*jade",['jade-compile'])
+	gulp.watch('./app/public/*css',['prefix'])
 
 })
-/*gulp.task('jade-compile',function(){
-	gulp.src('./app/page/*jade').pipe(jade({pretty:true})).pipe(gulp.dest("./app/pagehtml/"))
-})
-*/                      
 
-
-gulp.task('default',['style-compile',"jade-compile","wach"]);
-
- 
+                   
+gulp.task('default',['style-compile','jade-compile','wach'])
